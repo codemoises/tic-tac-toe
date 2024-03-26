3578fc3c396c3355034d0ac6e2e6759b316d6192
codemoises <88902469+codemoises@users.noreply.github.com>
2024-03-25 22:34:03 -0300

    gameContext com useContext e useReduce

--- src/contexts/GameContext.tsx
+++ src/contexts/GameContext.tsx
@@ -1,27 +1,96 @@
-import React, { createContext, useState } from 'react';
+import {
+  ReactNode,
+  createContext,
+  useCallback,
+  useContext,
+  useReducer,
+} from "react";
+import { GameContextActions } from "../enums/GameContextActions";
+
+export const INITIAL_STATE = {
+  squares: Array(9).fill(null),
+  isXNext: true,
+  gameResult: "",
+  history: [],
+};
+
+export type InitialStateType = typeof INITIAL_STATE;
 
 export const GameContext = createContext({
-    
+  state: INITIAL_STATE,
+  upDateSquare: () => {},
+  setIsXNext: () => {},
+  //   setGameResult: () => {},
 });
 
+export const useGameContext = () => {
+  const context = useContext(GameContext);
+
+  return context;
+};
+
 interface GameContextProps {
-    children: React.ReactNode;
+  children: ReactNode;
 }
 
-export default function GameContextProvider({ children }: GameContextProps) {
+interface IDispatchAction {
+  type: GameContextActions;
+  payload?: unknown;
+}
 
-    const [squares, setSquares] = useState(Array(9).fill(null));
-    const [isXNext, setIsXNext] = useState(true);
-    const [gameResult, setGameResult] = useState('');
+export const GameContextReducer = (
+  state = INITIAL_STATE,
+  action: IDispatchAction
+): InitialStateType => {
+  switch (action.type) {
+    case GameContextActions.upDateSquare: {
+      const { squares, history, isXNext, gameResult } = state;
 
-    const state = {
-        squares,
-        setSquares,
-        isXNext,
-        setIsXNext,
-        gameResult,
-        setGameResult,
+      const newHistory = [
+        ...history,
+        {
+          squares,
+          isXNext,
+          gameResult,
+        } as never,
+      ];
+
+      const newState = { ...state };
+      newState.squares = action.payload;
+      newState.isXNext = !isXNext;
+      newState.gameResult = gameResult;
+      newState.history = newHistory;
+
+      return newState;
+    }
+    case GameContextActions.setIsXNext:
+      return {
+        ...state,
+        isXNext: !state.isXNext,
       };
+    default:
+      throw new Error();
+  }
+};
 
-    return <GameContext.Provider value={state}>{children}</GameContext.Provider>;
+export default function GameContextProvider({ children }: GameContextProps) {
+  const [state, dispatch] = useReducer(GameContextReducer, INITIAL_STATE);
+
+  const upDateSquare = useCallback(() => {
+    dispatch({
+      type: GameContextActions.upDateSquare,
+    });
+  }, [dispatch]);
+
+  const setIsXNext = useCallback(() => {
+    dispatch({
+      type: GameContextActions.setIsXNext,
+    });
+  }, [dispatch]);
+
+  return (
+    <GameContext.Provider value={{ state, upDateSquare, setIsXNext }}>
+      {children}
+    </GameContext.Provider>
+  );
 }
